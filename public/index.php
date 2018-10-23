910085f09a838662ac36369e7ced5e244ab566b1
mapkoc88 <mjc93@njit.edu>
2018-10-22 20:08:26 -0400

    CSV File Code

--- public/index.php
+++ public/index.php
@@ -6,4 +6,20 @@
  * Time: 7:39 PM
  */
 
-echo 'Hola';
+main::start();
+
+class main {
+
+    static public function start(){
+
+        $file = fopen("project.csv", 'r');
+
+        while(! feof($file))
+        {
+            print_r(fgetcsv($file));
+        }
+
+        fclose($file);
+
+    }
+}
